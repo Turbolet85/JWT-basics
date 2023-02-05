2d4fdcf143379d4564d83c6265a982bb2d1f23f4
Anton Soldatov <turbolet85@gmail.com>
2023-02-05 22:25:51 +0100

    complete with token

---
+++ errors/index.js
@@ -0,0 +1,5 @@
+const CustomAPIError = require('./custom-error');
+const BadRequest = require('./bad-request');
+const UnauthentificatedError = require('./unauthetificated');
+
+module.exports = { CustomAPIError, BadRequest, UnauthentificatedError };
